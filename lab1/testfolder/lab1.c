ddc058af351cfb6f0db4a2bacc7fc58d1446949d
Przemyslaw Lasecki <przemyslaw.lasecki@edu.turkuamk.fi>
2025-01-29 18:23:43 +0200

    "Exercise completed made a native build and also build in vscode which worked with breakpoints"

---
+++ lab1/testfolder/lab1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    int age;
+
+    printf("Whats your age: ");
+
+    scanf("%d", &age);
+//b
+    printf("You are %d years old.\n", age);
+
+    printf("Hello \n");
+
+//b
+    printf("Hello \n");
+    
+    return 0;
+
+}
