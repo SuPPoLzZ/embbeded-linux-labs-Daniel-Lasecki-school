84e7266bdb1339df7ebef2631fd0c16fd7445ce5
Przemyslaw Lasecki <przemyslaw.lasecki@edu.turkuamk.fi>
2025-01-30 10:26:49 +0200

    compiled hello file for pi and it succesfully ran it on pi arm processor

--- lab2/hello.c
+++ lab2/hello.c
@@ -2,12 +2,12 @@
 #include <time.h>
 
 int main(){
-    FILE* output = fopen("output.txt", "a");
+    FILE* output = fopen("log_file.txt", "a");
 
     time_t t = time(NULL);
     struct tm tm_info = *localtime(&t);
 
-    fprintf(output, "Hello %04d-%02d-%02d %02d:%02d:%02d\n", 
+    fprintf(output, "Hello, i hope this runs at first try on arm processor %04d-%02d-%02d %02d:%02d:%02d\n", 
             tm_info.tm_year + 1900, tm_info.tm_mon + 1, tm_info.tm_mday,
             tm_info.tm_hour, tm_info.tm_min, tm_info.tm_sec);
 
