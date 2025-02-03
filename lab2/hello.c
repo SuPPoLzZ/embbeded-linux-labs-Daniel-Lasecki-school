a0576e648eea866d2fcc9b19e39daa5836acaf9d
Przemyslaw Lasecki <przemyslaw.lasecki@edu.turkuamk.fi>
2025-02-03 10:24:07 +0200

    lab2 completed

--- lab2/hello.c
+++ lab2/hello.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
-#include <time.h>
-
-int main(){
-    FILE* output = fopen("log_file.txt", "a");
-
-    time_t t = time(NULL);
-    struct tm tm_info = *localtime(&t);
-
-    fprintf(output, "Hello, i hope this runs at first try on arm processor %04d-%02d-%02d %02d:%02d:%02d\n", 
-            tm_info.tm_year + 1900, tm_info.tm_mon + 1, tm_info.tm_mday,
-            tm_info.tm_hour, tm_info.tm_min, tm_info.tm_sec);
-
-    fclose(output);
-    return 0;
+int main()
+{
+    FILE *f;
+    f = fopen("lab2.log", "a+"); 
+    if (f == NULL) { }
+    printf("Hello to stdout\n");
+    for(int i=0;i<4;i++) {
+        fprintf(f, "I'm logging %i ...\n",i);
+    }
 }
