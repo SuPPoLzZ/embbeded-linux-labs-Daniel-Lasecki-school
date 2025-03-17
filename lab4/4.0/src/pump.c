edf5392043b495df26aad4a58d43c3d51db2f89e
Przemyslaw Lasecki <przemyslaw.lasecki@edu.turkuamk.fi>
2025-03-17 10:01:27 +0200

    lab4.0 done

--- lab4/4.0/src/pump.c
+++ lab4/4.0/src/pump.c
@@ -1,64 +1,44 @@
 #include <stdio.h>
 #include <gpiod.h>
-#include <unistd.h>
-#include <time.h>
 
-#define CONSUMER "LED_ON"
-#define LOG_FILE "water_log.txt"
+int main() {
+    char *chipname = "gpiochip0";  
+    unsigned int line_num = 17;   // GPIO 17 is the input for detecting the LED state
+    struct gpiod_chip *chip;
+    struct gpiod_line *line;
+    int ret, val;
 
-
-
-void log_message(const char *message){
-    FILE *log_file = fopen(LOG_FILE, "a");
-    if (log_file != NULL){
-        time_t t = time(NULL);
-        struct tm *tm_info = localtime(&t);
-        fprintf(log_file, "[%s] %s\n", asctime(tm_info), message);  // Write timestamp and message
-        fclose(log_file);  
-    } else {
-        perror("Failed to open log file");
+    // Open the GPIO chip
+    chip = gpiod_chip_open_by_name(chipname);
+    if (!chip) {
+        perror("Open chip failed");
+        return 1;
     }
 
-}
-
-
-int main(int argc, char **argv)
-{
-	char *chipname = "gpiochip0";
-	unsigned int line_num = 23;	// GPIO Pin #23
-	struct gpiod_chip *chip;
-	struct gpiod_line *line;
-	int i, ret, val;
-    time_t t;
-    struct tm *tm_info;
-
-	chip = gpiod_chip_open_by_name(chipname);
-	if (!chip) {
-		perror("Open chip failed\n");
-		goto end;
+	// Request GPIO 17 as input
+	line = gpiod_chip_get_line(chip, 17);
+	ret = gpiod_line_request_input(line, "LED Detection");
+	if (ret < 0) {
+		perror("Failed to request GPIO 17 as input");
+		gpiod_chip_close(chip);
+		return 1;
 	}
 
-	line = gpiod_chip_get_line(chip, line_num);
-	if (!line) {
-		perror("Get line failed\n");
-		goto close_chip;
+	// Read the value of GPIO 17
+	val = gpiod_line_get_value(line);
+	if (val == -1) {
+		perror("Failed to read GPIO 17");
+	} else {
+		if (val == 1) {
+			printf("WATER PUMP ON\n");
+		} else {
+			printf("WATER PUMP OFF\n");
+		}
 	}
 
-	// CHECK GPIO IF ITS HIGH OR LOW
-    val = gpiod_line_get_value(line);
-	gpiod_line_release(line);
-    if (val == 1){
-		log_message("WATER LEVEL HIGH PUMP ON");
-		printf("WATER LEVEL HIGH PUMP ON");
-	}else if(val == 0)
-	{
-		log_message("WATER LEVEL LOW PUMP OFF");
-		printf("WATER LEVEL LOW PUMP OFF");
-	}else{
-		log_message("ERROR");
-	}
-close_chip:
-	gpiod_chip_close(chip);
-end:
-	return 0;
+    // Release the line and close the chip
+    gpiod_line_release(line);
+    gpiod_chip_close(chip);
+
+    return 0;
 }
